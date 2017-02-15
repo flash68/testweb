e5d454c1d77d2515c99605084be1b89d9e503a3b
flash68 <chernov-au@yandex.ru>
2017-02-15 19:17:55 +0300

    Add files via upload

--- src/components/Reg.jsx
+++ src/components/Reg.jsx
@@ -1,17 +1,20 @@
+/**
+ * Created by Алексей on 15.02.2017.
+ */
 import React from 'react';
 
-class MainView extends React.Component {
+class Reg extends React.Component {
     render() {
-        return (<div className="userloginform" id="0">
+        return (<div className="regform" id="2">
+
                 Логин:<br />
             <input className="login" ref="login" type="text" /><br />
                 Пароль: <br />
             <input className="pass" ref="pass" type="Password" /><br />
             <br />
-            <input className="vhod" onClick={this.handleClick} type="submit" value="Войти" /> <br />
             <input className="reg" onClick={this.handleClick} type="submit" value="Регистрация" /> <br />
         </div>);
     }
 }
 
-export default MainView;
+export default Reg;
