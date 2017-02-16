379051e86cdafef45ee7b70164d3d0de5f2f8840
flash68 <chernov-au@yandex.ru>
2017-02-16 18:02:48 +0300

    Delete LoginForm.jsx

--- src/components/LoginForm.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-class LoginForm extends React.Component {
-    render() {
-        return (<div className="loginform">Вы вошли как { this.state.Rol }!</div>);
-    }
-}
-export default LoginForm;
