399fdd4bee60ecbe34ff219c9021a149daed78b2
flash68 <chernov-au@yandex.ru>
2017-02-16 18:02:32 +0300

    Delete index.jsx

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import MainView from './components/MainView';
-import LoginForm from './components/LoginForm';
-import Reg from './components/Reg';
-
-render(<LoginForm />,
-    document.getElementById('root'));
