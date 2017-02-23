29f854eef139cbdecf1c92371f2789b455f670c8
flash68 <chernov-au@yandex.ru>
2017-02-23 10:54:07 +0300

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+/* Собирает все */
+import React from 'react';
+import { render } from 'react-dom';
+
+
+import Privetstvie from './components/Privetstvie';
+
+
+render(<Privetstvie />, document.getElementById('root'));
+
