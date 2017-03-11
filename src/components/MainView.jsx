7e281d1c546ec3e94bb9690f886037a2b8011b56
flash68 <chernov-au@yandex.ru>
2017-03-11 20:35:59 +0300

    Add files via upload

---
+++ src/components/MainView.jsx
@@ -0,0 +1,62 @@
+/** Главная форма**/
+import React from 'react';
+
+
+export default class MainView extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = ({
+            id: 1,
+            valueLogin: '',
+            valuePass: '',
+        });
+        this.handleUserChange = this.handleUserChange.bind(this);
+        this.handleUsersChange = this.handleUsersChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleUserChange(event) {
+        this.setState({
+            valueLogin: event.target.value,
+        });
+    }
+    handleUsersChange(event) {
+        this.setState({
+            valuePass: event.target.value,
+        });
+    }
+
+    handleSubmit(event) {
+        if (this.state.valuePass == this.props.UserFormAdmins[0].pass && this.state.valueLogin == this.props.UserFormAdmins[0].name) {
+            this.props.OnVhod();
+        } else {
+            this.setState({
+                valueLogin: '',
+                valuePass: '',
+            });
+            alert('Введите еще раз!');
+        }
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="userloginform">
+                <label>
+                        Логин:<br />
+                    <input name="login" value={this.state.valueLogin} type="text" onChange={this.handleUserChange} /><br />
+                </label>
+                <label> Пароль: <br />
+                    <input
+                        name="pass" value={this.state.valuePass} type="Password"
+                        onChange={this.handleUsersChange}
+                    />
+                    <br />
+                    <br /></label>
+                <input className="vhod" onClick={this.handleSubmit} type="submit" value="Войти" /> <br />
+                <input className="reg" onClick={this.props.OnReg} type="button" value="Регистрация" /> <br />
+            </div>
+        );
+    }
+
+}
