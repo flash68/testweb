3e17fddb7bb8604d5cc4c04a38963e1ccc302836
flash68 <chernov-au@yandex.ru>
2017-04-01 15:23:13 +0300

    Add files via upload

---
+++ src/components/Privetstvie.jsx
@@ -0,0 +1,70 @@
+/* Собирает все */
+import React from 'react';
+
+import MainView from './MainView';
+import Reg from './Reg';
+import LoginForm from './LoginForm';
+
+const UserFormAdmins = [{
+    UserId: 0,
+    name: 'admin',
+    pass: 'admin',
+}];
+
+class Privetstvie extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            id: 1,
+            valueLogin: '',
+            valuePass: '',
+        };
+        this.onReg = this.onReg.bind(this);
+        this.onVhod = this.onVhod.bind(this);
+    }
+
+    onReg() {
+        this.setState({
+            id: 2,
+        });
+    }
+
+    onVhod(user) {
+        this.setState({
+            id: 3,
+            User: {
+                valueLogin: user.login,
+                valuePass: user.pass,
+            },
+        });
+    }
+    render() {
+        if (this.state.id === 2) {
+            return (
+                <div>
+                    <Reg
+                        onReg={this.onReg}
+                        onVhod={this.onVhod}
+                    />
+                </div>);
+        } else if (this.state.id === 3) {
+            return (
+                <LoginForm
+                    User={this.state.User}
+                    onVhod={this.onVhod}
+                    valueLogin={this.state.valueLogin}
+                />
+            );
+        }
+        return (
+            <MainView
+                onReg={this.onReg}
+                onVhod={this.onVhod}
+                UserFormAdmins={UserFormAdmins}
+            />
+        );
+    }
+}
+
+export default Privetstvie;
